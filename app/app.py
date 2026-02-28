4111686ca4b629524cdfbb9e76df594218a3f676
monica <monajhala1910@gmail.com>
2026-02-28 13:21:36 +0530

    Update home route response message

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Assignment - Docker Container Running!"
+    return "DevOps Assignment - Docker Running!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
